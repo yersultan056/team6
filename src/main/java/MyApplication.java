152aab60bc39b997e8641c31ee368d360af155d3
yersultan056 <230129@astanait.edu.kz>
2024-02-13 21:04:09 +0600

    Connection to db 2

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -1,15 +1,31 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MyApplication {
     public static void main(String[] args){
         String connectionString = "jdbc:postgresql://localhost:5432/team6";
-
+        Connection con = null;
         try{
-            Connection con = DriverManager.getConnection(connectionString, "postgres", "5859");
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(connectionString, "postgres", "5859");
+
+            String sql = "SELECT id, name, surname, gender, rights_category FROM users ORDER BY rights_category ASC";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getString("name"));
+            }
         } catch (SQLException e){
             System.out.println("connection error: " + e.getMessage());
+        } catch (ClassNotFoundException e){
+            System.out.println("driver error: " + e.getMessage());
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.out.println("could not close the connection: " + e.getMessage());
+                }
+            }
         }
-    }
+        }
 }
