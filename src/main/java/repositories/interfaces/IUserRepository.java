48d07e3227bd52cbea94b0dec4dae2ee930611a2
Rakhman <231355@astanait.edu.kz>
2024-02-14 04:08:33 +0600

    Coments

--- src/main/java/repositories/interfaces/IUserRepository.java
+++ src/main/java/repositories/interfaces/IUserRepository.java
@@ -5,7 +5,16 @@ import modules.User;
 import java.util.List;
 
 public interface IUserRepository {
+    // Attempts to create a new user in the database.
+    // Returns true if successful, false otherwise.
     boolean createUser(User user);
+
+    // Retrieves a user by their unique identifier (id) from the database.
+    // Returns a User object if found, null otherwise.
     User getUser(int id);
+
+    // Retrieves a list of all users from the database.
+    // Returns a list of User objects, which may be empty if no users are found.
     List<User> getAllUsers();
 }
+
