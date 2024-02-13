edc40ef7ba5b5715d86e8f667ec70b335e3cefd3
Rakhman <231355@astanait.edu.kz>
2024-02-13 20:58:11 +0600

    test init

---
+++ src/main/java/Users.java
@@ -0,0 +1,49 @@
+public class Users {
+    private int id;
+    private String name;
+    private String surname;
+    private boolean gender;
+    private String rightsCategory;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getRightsCategory() {
+        return rightsCategory;
+    }
+
+    public void setRightsCategory(String rightsCategory) {
+        this.rightsCategory = rightsCategory;
+    }
+}
+
+
