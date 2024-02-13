1b28f41c09bffa50eec939e54f9b96a37fce6a6b
yersultan056 <230129@astanait.edu.kz>
2024-02-13 22:11:32 +0600

    Connected to db 3

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -1,8 +1,12 @@
 import java.sql.*;
+import java.util.ArrayList;
+
+import modules.User;
 
 public class MyApplication {
     public static void main(String[] args){
         String connectionString = "jdbc:postgresql://localhost:5432/team6";
+        ArrayList<User> usersar = new ArrayList<>();
         Connection con = null;
         try{
             Class.forName("org.postgresql.Driver");
@@ -12,7 +16,13 @@ public class MyApplication {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
-                System.out.println(rs.getString("name"));
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String surname = rs.getString("surname");
+                boolean gender = rs.getBoolean("gender");
+                String rights_category = rs.getString("rights_category");
+
+                User user = new User(id, name, surname, gender, rights_category);
             }
         } catch (SQLException e){
             System.out.println("connection error: " + e.getMessage());
@@ -26,6 +36,9 @@ public class MyApplication {
                     System.out.println("could not close the connection: " + e.getMessage());
                 }
             }
+        }
+        for (User user : usersar) {
+            System.out.println(user);
         }
         }
 }
