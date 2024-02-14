d220350df48a0259b567bc06e44e7f82b7c07dfc
Zharas <231237@astanait.edu.kz>
2024-02-14 06:10:01 +0600

    time

--- src/main/java/modules/TimeSlot.java
+++ src/main/java/modules/TimeSlot.java
@@ -1,43 +1,45 @@
 package modules;
 
-
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-    public class TimeSlot {
-        private LocalDateTime startTime;
-        private LocalDateTime endTime;
-        private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+public class TimeSlot {
+    private LocalDateTime startTime; // Start time of the time slot
+    private LocalDateTime endTime; // End time of the time slot
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"); // Formatter for formatting date and time
 
-        public TimeSlot(LocalDateTime startTime, LocalDateTime endTime) {
-            this.startTime = startTime;
-            this.endTime = endTime;
-        }
-
-        public LocalDateTime getStartTime() {
-            return startTime;
-        }
-
-        public void setStartTime(LocalDateTime startTime) {
-            this.startTime = startTime;
-        }
-
-        public LocalDateTime getEndTime() {
-            return endTime;
-        }
-
-        public void setEndTime(LocalDateTime endTime) {
-            this.endTime = endTime;
-        }
-
-        @Override
-        public String toString() {
-            return "TimeSlot{" +
-                    "startTime=" + startTime.format(formatter) +
-                    ", endTime=" + endTime.format(formatter) +
-                    '}';
-        }
+    // Constructor of the class, takes start time and end time of the time slot
+    public TimeSlot(LocalDateTime startTime, LocalDateTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
+    // Getter for the start time of the time slot
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
 
+    // Setter for the start time of the time slot
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    // Getter for the end time of the time slot
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    // Setter for the end time of the time slot
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    // Override of the toString method to provide custom string representation of the object
+    @Override
+    public String toString() {
+        return "TimeSlot{" +
+                "startTime=" + startTime.format(formatter) +
+                ", endTime=" + endTime.format(formatter) +
+                '}';
+    }
+}
